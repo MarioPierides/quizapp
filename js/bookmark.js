ff3dcc1ddd878eba8fa316ea55b7afa890e2b28f
Mario Pierides <mario.pierides@gmx.de>
2022-04-18 18:37:51 +0200

    new profile page and javascript function

--- js/bookmark.js
+++ js/bookmark.js
@@ -1,15 +1,15 @@
-// document.addEventListener("DOMContentLoaded", function () {
-//   const bookmarks = document.querySelectorAll('[data-js="bookmark"]');
+document.addEventListener("DOMContentLoaded", function () {
+  const bookmarks = document.querySelectorAll('[data-js="bookmark"]');
 
-//   bookmarks.forEach(function (bookmark) {
-//     bookmark.addEventListener("click", function (e) {
-//       e.currentTarget.classList.toggle("bookmark-active");
-//     });
-//   });
-// });
-
-const bookmarkToggle = document.querySelector('[data-js="bookmarkToggle"]');
-
-bookmarkToggle.addEventListener("click", () => {
-  bookmarkToggle.classList.toggle("bookmark-active");
+  bookmarks.forEach(function (bookmark) {
+    bookmark.addEventListener("click", function (e) {
+      e.currentTarget.classList.toggle("bookmark-active");
+    });
+  });
 });
+
+// const bookmarkToggle = document.querySelector('[data-js="bookmarkToggle"]');
+
+// bookmarkToggle.addEventListener("click", () => {
+//   bookmarkToggle.classList.toggle("bookmark-active");
+// });
